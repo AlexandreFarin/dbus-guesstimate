aeec1c687da7a7c7bc47d0e9bda069f3f73c61ff
Alex <alexandre.farin@databricks.com>
2023-06-19 18:09:09 +0200

    Finalize the UI and change the model

--- app.py
+++ app.py
@@ -1,65 +1,6 @@
-import os
-import requests
-import json
-import pandas as pd
-from databricks import sql
-from dash import Dash, dash_table, html, dcc, Output, Input
 import dash_bootstrap_components as dbc
-from dotenv import load_dotenv
-
-load_dotenv()
-
-data = {
-  "dataframe_split": {
-    "columns": [
-      "pct_eda",
-      "pct_etl",
-      "pct_edw",
-      "pct_jobs",
-      "DailyGbProcess",
-      "DeltaPercent",
-      "activeUsers",
-      "ServerlessPercent",
-      "Industry",
-      "customerAgeMonths"
-    ],
-    "data": [
-      [
-        0,
-        0,
-        0.99,
-        0,
-        7,
-        0.86,
-        22,
-        0,
-        "Professional, Scientific, and Technical Services",
-        45
-      ]
-    ]
-  }
-}
-def get_dataset(table_name):
-    with sql.connect(server_hostname=os.getenv("db_host"),
-                    http_path=os.getenv("http_path"),
-                    access_token=os.getenv("token")) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT * FROM users.alexandre_farin.{table_name}")
-            result = cursor.fetchall()
-            columns = [desc[0] for desc in cursor.description]
-            df = pd.DataFrame(result, columns=columns)
-    return df
-
-def score_model(data):
-    headers = {
-       "Authorization": f'Bearer {os.getenv("token")}',
-       "Content-Type": "application/json",
-    }
-    model_uri=f'https://{os.getenv("db_host")}/serving-endpoints/dus-guesstimate/invocations'
-    response = requests.post(headers=headers, url=model_uri, json=data)
-    if response.status_code != 200:
-        raise Exception(f"Request failed with status {response.status_code}, {response.text}")
-    return response.json()["predictions"][0]
+from dash import Dash, html, dcc, Output, Input, State
+from utils import get_dataset, score_model
 
 df = get_dataset("customers_last_month")
 
@@ -93,7 +34,16 @@ app.layout = html.Div(
                             className="blockquote-footer")
         ]
       ),
-      width={"size": 4, "offset": 4})
+      width={"size": 6, "offset": 3})
+    ),
+    dbc.Row(
+      dbc.Button("Predict", className="btn btn-danger", id="prediction-button", n_clicks=0, disabled=False),
+      className="d-grid gap-2 col-4 mx-auto"
+    ),
+    html.Br(),  
+    dbc.Row(
+       html.H3(style={'text-align':'center'},id="prediction-output"),
+       className="d-grid gap-2 col-4 mx-auto"
     ),
     dbc.Row(
         [
@@ -106,45 +56,173 @@ app.layout = html.Div(
                   ),
                   html.Div(html.Img(src='assets/company.svg'),
                            style={'textAlign': 'center'}),
-                  html.Div(
+                  dbc.Form([
+                      html.Div(
                         [
-                          dbc.Label("Dropdown", html_for="dropdown"),
-                          dcc.Dropdown(df['Industry'].unique(), id="dropdown"
-                          ),
+                          html.P("Customer seniority (in months)"),
+                          dbc.Input(type="number", min=0, max=200, step=1, value=df['customerAgeMonths'].median(), id="age", className="form-control"),
                         ],
-                        className="mb-3")
+                        className="mb-3"),
+                      html.Div(
+                        [
+                          html.P("Number of active users"),
+                          dbc.Input(type="number", min=0, max=1000, step=1, value=df['activeUsers'].median(), id="active_users", className="form-control"),
+                        ],
+                        className="mb-3"),
+                      html.Div(
+                        [
+                          html.P("Industry"),
+                          dbc.Select(
+                          id="industry", value=df["Industry"].mode()[0],
+                          options=[{"value":v, "label":v} for v in df['Industry'].unique()]
+                        ),
+                        ],
+                        className="mb-3"
+                      )
+                  ],style = {'margin-left':'5px', 'margin-top':'5px', 'margin-right':'5px'})
               ],className="card mb-3")
           ),
 
           dbc.Col(
               html.Div([
-                  html.H3("Data Preparation", className="card-header"),
+                  html.H3("Data Engineering", className="card-header"),
                   html.Div(
                      html.H6("cfrfrtftgt", className="card-subtitle text-muted"),
                      className="card-body"
                   ),
-                  html.Div([
-                    dbc.Label("Email", html_for="example-email"),
-                    dbc.Input(type="email", id="example-email", placeholder="Enter email"),
-                    dbc.FormText(
-                        "Are you on email? You simply have to be these days",
-                        color="secondary",
-                    ),
-                  ], className="mb-3")
+                  html.Div(html.Img(src='assets/data-eng.svg'),
+                           style={'textAlign': 'center'}),
+                  dbc.Form([
+                     html.Div(
+                        [
+                          html.P("ETL usage in %"),
+                          dbc.Input(type="number", min=0, max=100, step=1, value=70, id="etl_pct", className="form-control", invalid=False),
+                        ],
+                        className="mb-3"),
+                      html.Div(
+                        [
+                          html.P("Data volume processed daily"),
+                          dbc.Select(
+                          id="data_volume",
+                          value=df["DailyGbProcessCat"].mode()[0],
+                          options=[
+                              {"value": "0-10 G", "label": "Less than 10 Giga"},
+                              {"value": "10-100 G", "label": "10 - 100 Giga"},
+                              {"value": "100-500 G", "label": "100 - 500 Giga"},
+                              {"value": "500-1000 G", "label": "500 Giga - 1 Tera"},
+                              {"value": "1-10 T", "label": "1 - 10 Tera"},
+                              {"value": "+10 T", "label": "More than 10 Tera"},
+                          ]
+                        ),
+                        ],
+                        className="mb-3"
+                      ),
+                      html.Div(
+                        [
+                          html.P("Percentage jobs"),
+                          dcc.Slider(id="jobs", min=0, max=100, step=10, value=round(df["pct_jobs"].median(),1) * 100),
+                        ],
+                      className="mb-3"),
+                      html.Div(
+                        [
+                          html.P("Delta usage"),
+                          dcc.Slider(id="delta", min=0, max=100, step=10, value=round(df["DeltaPercent"].median(),1) * 100),
+                        ],
+                      className="mb-3")
+                  ],style = {'margin-left':'5px', 'margin-top':'5px', 'margin-right':'5px'})
               ],className="card mb-3")
           ),
 
-          dbc.Col(html.Div("One of four columns")),
+          dbc.Col(
+              html.Div([
+                  html.H3("Data Science / ML", className="card-header"),
+                  html.Div(
+                     html.H6("cfrfrtftgt", className="card-subtitle text-muted"),
+                     className="card-body"
+                  ),
+                  html.Div(html.Img(src='assets/data-science.svg'),
+                           style={'textAlign': 'center'}),
+                  dbc.Form([
+                     html.Div(
+                        [
+                          html.P("ML usage in %"),
+                          dbc.Input(type="number", min=0, max=100, step=1, value=10, id="ml_pct", className="form-control", invalid=False),
+                        ],
+                        className="mb-3"),
+                  ],style = {'margin-left':'5px', 'margin-top':'5px', 'margin-right':'5px'})
+              ],className="card mb-3")
+          ),
 
-          dbc.Col(html.Div("One of four columns")),
-        ]
-    ),
-        dbc.Row(
-        dbc.Col(html.H1(children=score_model(data),
-                       style={'text-align':'center'}))
+          dbc.Col(
+              html.Div([
+                  html.H3("Data Analysis", className="card-header"),
+                  html.Div(
+                     html.H6("cfrfrtftgt", className="card-subtitle text-muted"),
+                     className="card-body"
+                  ),
+                  html.Div(html.Img(src='assets/data-analysis.svg'),
+                           style={'textAlign': 'center'}),
+                  dbc.Form([
+                     html.Div(
+                        [
+                          html.P("SQL usage in %"),
+                          dbc.Input(type="number", min=0, max=100, step=1, value=20, id="sql_pct", className="form-control", invalid=False),
+                        ],
+                        className="mb-3"),
+                    html.Div(
+                        [
+                          html.P("Serverless usage"),
+                          dcc.Slider(id="serverless", min=0, max=100, step=10 , value=round(df["ServerlessPercent"].median(),1) * 100),
+                        ],
+                      className="mb-3")
+                  ],style = {'margin-left':'5px', 'margin-top':'5px', 'margin-right':'5px'})
+              ],className="card mb-3")
+          ),
+        ],
+        style = {'margin-left':'10px', 'margin-top':'7px', 'margin-right':'10px'}
     ),
   ]
 )
+
+@app.callback(
+      Output('prediction-output', 'children'),
+      [
+        Input('prediction-button', 'n_clicks'),
+        State('age', 'value'),
+        State('active_users', 'value'),
+        State('industry', 'value'),
+        State('etl_pct', 'value'),
+        State('data_volume', 'value'),
+        State('jobs', 'value'),
+        State('delta', 'value'),
+        State('ml_pct', 'value'),
+        State('sql_pct', 'value'),
+        State('serverless', 'value')
+      ]
+    )
+def predict(n_clicks, age, active_users, industry, etl_pct, data_volume, jobs, delta, ml_pct, sql_pct, serverless):
+    if n_clicks == 0:
+        return ""
+    else:
+        dbus = score_model(age, active_users, industry, etl_pct/100, data_volume, jobs/100, delta/100, ml_pct/100, sql_pct/100, serverless/100)
+        return f"{round(dbus/10)*10} DBUs / month"
+
+@app.callback(
+      Output('etl_pct', 'invalid'),
+      Output('ml_pct', 'invalid'),
+      Output('sql_pct', 'invalid'),
+      Output('prediction-button', 'disabled'),
+      [
+        Input('etl_pct', 'value'),
+        Input('ml_pct', 'value'),
+        Input('sql_pct', 'value')
+      ]
+    )
+def sum_is_100(etl_pct, ml_pct, sql_pct):
+    if etl_pct + ml_pct + sql_pct == 100:
+        return False, False, False, False
+    else:
+        return True, True, True, True
 
 if __name__=='__main__':
     app.run_server(debug=True)
---
+++ utils.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import requests
+import pandas as pd
+from databricks import sql
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+
+load_dotenv()
+
+def get_dataset(table_name):
+    with sql.connect(server_hostname=os.getenv("db_host"),
+                    http_path=os.getenv("http_path"),
+                    access_token=os.getenv("token")) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM users.alexandre_farin.{table_name}")
+            result = cursor.fetchall()
+            columns = [desc[0] for desc in cursor.description] 
+    return pd.DataFrame(result, columns=columns)
+
+def score_model(age, active_users, industry, etl_pct, data_volume, jobs, delta, ml_pct, sql_pct, serverless):
+    headers = {
+       "Authorization": f'Bearer {os.getenv("token")}',
+       "Content-Type": "application/json",
+    }
+    input_data = {
+      "dataframe_split": {
+        "columns": [
+          "pct_eda",
+          "pct_etl",
+          "pct_edw",
+          "pct_jobs",
+          "DailyGbProcessCat",
+          "DeltaPercent",
+          "activeUsers",
+          "ServerlessPercent",
+          "Industry",
+          "customerAgeMonths"
+        ],
+        "data": [
+          [
+            ml_pct,
+            etl_pct,
+            sql_pct,
+            jobs,
+            data_volume,
+            delta,
+            active_users,
+            serverless,
+            industry,
+            age
+          ]
+        ]
+      }
+    }
+    model_uri=f'https://{os.getenv("db_host")}/serving-endpoints/dus-guesstimate/invocations'
+    response = requests.post(headers=headers, url=model_uri, json=input_data, timeout=20)
+    
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status {response.status_code}, {response.text}")
+    
+    dbus = response.json()["predictions"][0]
+    return int(dbus)
